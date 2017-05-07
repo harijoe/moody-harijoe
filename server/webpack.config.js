a7e2c66c889bb232b6f267f0b65a669789da1f59
Julien Vallini <vallinij@gmail.com>
2017-05-07 14:37:36 +0200

    Server

---
+++ server/webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  entry: './index',
+  output: {
+    path: __dirname,
+    filename: 'handler.js',
+    libraryTarget: 'commonjs2'
+  },
+  target: 'node',
+  externals: [require('webpack-node-externals')()],
+  resolve: {
+    extensions: ['.js']
+  },
+  plugins: [
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
+};
